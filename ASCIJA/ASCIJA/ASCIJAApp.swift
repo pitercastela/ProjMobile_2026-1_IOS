92e4c64046f49591fdee755bc694e716bb7ce9ce
LucasACastela <85808717+pitercastela@users.noreply.github.com>
2026-05-12 11:23:01 -0300

    Add files via upload

---
+++ ASCIJA/ASCIJA/ASCIJAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASCIJAApp.swift
+//  ASCIJA
+//
+//  Created by LUCAS ALVES CASTELA PEREIRA on 12/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ASCIJAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
